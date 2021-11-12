9aef9ff80a58bac781ab229f4c8d57f3a8691a0c
Carlos Tojal <carlos.tojal@hotmail.com>
2021-11-12 17:16:41 +0000

    example had been changed

--- Formula/example.py
+++ Formula/example.py
@@ -2,9 +2,6 @@ from Controls import Controls
 
 controls = Controls()
 
-# 10400 -> 20
-
-for i in range(0, 1):
-    controls.throttle(0.76)
-    controls.brake(0.10)
-    controls.clutch(0.65)
+controls.throttle(0.76)
+controls.brake(0.10)
+controls.clutch(0.65)

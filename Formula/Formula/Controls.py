67a2452ee8e0819f5fbf5569ba942f6b9ea8c71f
Carlos Tojal <carlos.tojal@hotmail.com>
2021-11-16 09:58:02 +0000

    implement rest of smooth controls. change from time based step to value interval based step (makes more sense and is smoother).

--- Formula/Formula/Controls.py
+++ Formula/Formula/Controls.py
@@ -3,12 +3,7 @@ import rospy
 from std_msgs.msg import Float64, Int8
 from enum import Enum
 from time import sleep
-import datetime
 from threading import Thread
-import multiprocessing
-
-# smaller this value, more gradual will be value changes
-# with the compromise of worse performance
 
 class AvailableControls(Enum):
     """
@@ -24,7 +19,9 @@ class AvailableControls(Enum):
 
 class Controls:
 
-    STEP_TIME = 1
+    # smaller this value, more gradual will be value changes
+    # with the compromise of worse performance
+    STEP_LEN = 0.01
 
     def __init__(self, wait_time=0.0):
         print("Initializing controls.")
@@ -45,7 +42,14 @@ class Controls:
         # ex: car was instructed to steer left, but meanwhile is instructed to steer right
         # in this case, the thread that was started to steer left will be stopped
         self.current_steering_thread = None
+        self.current_throttle_thread = None
+        self.current_brake_thread = None
+        self.current_clutch_thread = None
+
         self.stop_steering_thread = False
+        self.stop_throttle_thread = False
+        self.stop_brake_thread = False
+        self.stop_clutch_thread = False
 
         # this declares this instance as a rospy node
         rospy.init_node('controls', anonymous=True)
@@ -65,17 +69,32 @@ class Controls:
         self.steering_pub.unregister()
         print("Controls unregistered.")
 
-    def clutch(self, value):
-        self.current_clutch_value = value
-        self.clutch_pub.publish(value)
+    def clutch(self, value, update_time=0.0):
 
-    def brake(self, value):
-        self.current_brake_value = value
-        self.brake_pub.publish(value)
+        if update_time != 0.0:
+            thread = Thread(target=self.gradual_value_update, args=(value, update_time, AvailableControls.Clutch))
+            thread.start()
+        else:
+            self.current_clutch_value = value
+            self.clutch_pub.publish(value)
+
+    def brake(self, value, update_time=0.0):
+
+        if update_time != 0.0:
+            thread = Thread(target=self.gradual_value_update, args=(value, update_time, AvailableControls.Brake))
+            thread.start()
+        else:
+            self.current_brake_value = value
+            self.brake_pub.publish(value)
     
-    def throttle(self, value):
-        self.current_throttle_value = value
-        self.throttle_pub.publish(value)
+    def throttle(self, value, update_time=0.0):
+        
+        if update_time != 0.0:
+            thread = Thread(target=self.gradual_value_update, args=(value, update_time, AvailableControls.Throttle))
+            thread.start()
+        else:
+            self.current_throttle_value = value
+            self.throttle_pub.publish(value)
 
     def gearUp(self):
         self.gears_pub.publish(1)
@@ -83,10 +102,10 @@ class Controls:
     def gearDown(self):
         self.gears_pub.publish(-1)
 
-    def steering(self, value, time=0.0):
+    def steering(self, value, update_time=0.0):
 
-        if time != 0.0:
-            thread = Thread(target=self.gradual_value_update, args=(value, time, AvailableControls.Steering))
+        if update_time != 0.0:
+            thread = Thread(target=self.gradual_value_update, args=(value, update_time, AvailableControls.Steering))
             thread.start()
         else:
             self.current_steering_value = value
@@ -95,26 +114,52 @@ class Controls:
         # 0 -> 1 = right
         # 0 -> -1 = left
 
-    def gradual_value_update(self, target_value, time, control=AvailableControls.None_):
+    def gradual_value_update(self, target_value, update_time, control=AvailableControls.None_):
 
         if control == AvailableControls.Steering:
 
-            if self.current_steering_thread is not None:
-                if self.current_steering_thread.is_alive():
-                    # wait for the existing thread to finish
-                    # (in the future it is a better idea to finish the thread)
-                    self.stop_steering_thread = True
-                    self.current_steering_thread.join()
+            if self.current_steering_thread is not None and self.current_steering_thread.is_alive():
+                self.stop_steering_thread = True
+                # have to wait for the existing thread (it will stop, we are just waiting that)
+                self.current_steering_thread.join()
 
             self.current_steering_thread = threading.current_thread()
-            
+
             val_diff = target_value - self.current_steering_value
+
+        elif control == AvailableControls.Throttle:
+
+            if self.current_throttle_thread is not None and self.current_throttle_thread.is_alive():
+                self.stop_throttle_thread = True
+
+            self.current_throttle_thread = threading.current_thread()
+
+            val_diff = target_value - self.current_throttle_value
+
+        elif control == AvailableControls.Brake:
+
+            if self.current_brake_thread is not None and self.current_brake_thread.is_alive():
+                self.stop_brake_thread = True
+
+            self.current_brake_thread = threading.current_thread()
+
+            val_diff = target_value - self.current_brake_value
+
+        elif control == AvailableControls.Clutch:
+
+            if self.current_clutch_thread is not None and self.current_clutch_thread.is_alive():
+                self.stop_clutch_thread = True
+
+            self.current_clutch_thread = threading.current_thread()
+
+            val_diff = target_value - self.current_clutch_value
+
         else:
-            print("Control not available.")
-            return
+            raise Exception("Control not available." + str(control))
         
         # number of steps it takes to change value
-        n_steps = time / self.STEP_TIME
+        n_steps = abs(val_diff) / self.STEP_LEN
+
 
         for step in range(int(n_steps)):
             if control == AvailableControls.Steering:
@@ -123,8 +168,25 @@ class Controls:
                     return
                 self.current_steering_value = self.current_steering_value + (val_diff / n_steps)
                 self.steering_pub.publish(self.current_steering_value)
+            elif control == AvailableControls.Throttle:
+                if self.stop_throttle_thread:
+                    self.stop_throttle_thread = False
+                    return
+                self.current_throttle_value = self.current_throttle_value + (val_diff / n_steps)
+                self.throttle_pub.publish(self.current_throttle_value)
+            elif control == AvailableControls.Brake:
+                if self.stop_brake_thread:
+                    self.stop_brake_thread = False
+                    return
+                self.current_brake_value = self.current_brake_value + (val_diff / n_steps)
+                self.brake_pub.publish(self.current_brake_value)
+            elif control == AvailableControls.Clutch:
+                if self.stop_clutch_thread:
+                    self.stop_clutch_thread = False
+                    return
+                self.current_clutch_value = self.current_clutch_value + (val_diff / n_steps)
+                self.clutch_pub.publish(self.current_clutch_value)
             else:
-                print("Control not available.")
-                return
+                raise Exception("Control not available." + str(control))
             
-            sleep(self.STEP_TIME)
+            sleep(update_time / n_steps)
--- Formula/example.py
+++ Formula/example.py
@@ -3,10 +3,8 @@ from Formula import Controls
 
 controls = Controls.Controls()
 
-controls.STEP_TIME = 0.1
-
-controls.steering(-0.5, time=2)
-controls.steering(0.5, time=2)
+controls.steering(-0.5, update_time=2)
+controls.steering(0.5, update_time=2)
 controls.throttle(0.76)
 controls.brake(0.10)
 controls.clutch(0.65)
